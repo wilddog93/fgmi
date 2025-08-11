ba16559356fab5e0bc5467c8ad21d9885a3fbf63
Jumakri Ridho Fauzi <ridhoajibx@gmail.com>
2025-08-11 20:30:22 +0700

    [ridho] - create new apps - WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,157 @@
+import { Button } from "@/components/ui/button";
+// import { Instagram, Sms } from "iconsax-reactjs";
+import { FaInstagram, FaSms, FaRegEnvelope, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <header className="row-start-1 flex gap-[24px] flex-wrap items-center justify-center">
         <Image
           className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/img/fgmi-header-logo.png"
+          alt="FGMI header logo"
           width={180}
           height={38}
           priority
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      </header>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center justify-center">
+        <Image
+          className="dark:invert"
+          src="/img/fgmi-logo.png"
+          alt="FGMI logo"
+          width={140}
+          height={38}
+          priority
+        />
+        <div className="font-mono list-inside list-decimal text-md/6 text-center">
+          <div className="mb-2 tracking-[-.01em] text-2xl sm:text-4xl font-bold">
+            We&lsquo;ll be back soon!
+          </div>
+          <div className="mb-2 tracking-[-.01em]">
+            Sorry for the inconvenience,<br/> we&lsquo;re performing some maintenance at the moment.<br/> We&lsquo;ll be back online shortly!
+          </div>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex gap-2 items-center flex-col">
+          <div className="flex gap-2 items-center flex-col sm:flex-row">
+            <Link
+              className="flex items-center gap-2 hover:cursor-pointer"
+              href="/register-bootcamp-geoscience"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button
+                variant='default'
+                className="flex items-center gap-2 hover:cursor-pointer rounded-2xl"
+              >
+                Bootcamp Geoscience
+              </Button>
+            </Link>
+          </div>
+
+          <div className="flex gap-2 items-center flex-col sm:flex-row">
+            <Link
+              className="flex items-center gap-2 hover:cursor-pointer"
+              href="https://mail.fgmi.or.id/mail/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button
+                variant='outline'
+                className="flex items-center gap-2 hover:cursor-pointer rounded-2xl min-w-[158px]"
+              >
+                FGMI Mail
+              </Button>
+            </Link>
+            <Link
+              className="flex items-center gap-2 hover:cursor-pointer"
+              href="/qr-generator"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button
+                variant='outline'
+                className="flex items-center gap-2 hover:cursor-pointer rounded-2xl min-w-[158px]"
+              >
+                QR Generator
+              </Button>
+            </Link>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+
+      {/* footer */}
+      <footer className="overflow-hidden row-start-3 flex flex-col gap-2 flex-wrap items-center justify-center text-xs">
+        {/* Social Media & Copyright */}
+        <div className="flex gap-2 items-center flex-col">
+          <div className="flex gap-1 flex-wrap items-center">
+            <Link 
+              href="mailto:info@fgmi.or.id"
+              className="flex items-center justify-center gap-2 p-0"
+            >
+              <Button
+                variant='link'
+                className="flex items-center gap-2 hover:cursor-pointer p-0 !px-1"
+              >
+                <FaRegEnvelope className="h-5 w-5 px-0" />
+              </Button>
+            </Link>
+            <Link 
+              href="https://instagram.com/fgmindo"
+              className="flex items-center justify-center gap-2"
+            >
+              <Button
+                variant='link'
+                className="flex items-center gap-2 hover:cursor-pointer p-0 !px-1"
+              >
+                <FaInstagram className="h-5 w-5 px-0" />
+              </Button>
+            </Link>
+            <Link 
+              href="https://www.linkedin.com/company/forum-geosaintis-muda-indonesia/"
+              className="flex items-center justify-center gap-2"
+            >
+              <Button
+                variant='link'
+                className="flex items-center gap-2 hover:cursor-pointer p-0 !px-1"
+              >
+                <FaLinkedin className="h-5 w-5 px-0" />
+              </Button>
+            </Link>
+            <Link 
+              href="https://wa.me/6282114563860"
+              className="flex items-center justify-center gap-2"
+            >
+              <Button
+                variant='link'
+                className="flex items-center gap-2 hover:cursor-pointer p-0 !px-1"
+              >
+                <FaWhatsapp className="h-5 w-5 px-0" />
+              </Button>
+            </Link>
+          </div>
+          <div className="font-normal text-center leading-snug font-sans text-ellipsis">
+            Copyright © 2025 Indonesian Young Geoscientists Forum. All rights reserved.
+          </div>
+        </div>
+
+        {/* Address */}
+        <Link 
+          className="flex items-center justify-center gap-2"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          href="https://maps.app.goo.gl/jjCpYFbpJxA7WR7x7"
+          >
+            <Button 
+              variant='link'
+              className="flex items-center gap-2 hover:cursor-pointer hover:no-underline p-0 !px-1 text-center text-inherit text-xs font-normal text-ellipsis"
+            >
+              SOHO Pancoran Tower Splendor – Lt.18 Unit 1817, Tebet Barat, Pancoran, Jakarta Selatan, DKI Jakarta 12810
+            </Button>
+          </Link>
       </footer>
     </div>
   );
---
+++ src/components/client/theme-mode-toggle.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function ThemeModeToggle() {
+  const { setTheme } = useTheme()
+
+  const mode = [
+    { value: "light", label: "Light"},
+    { value: "dark", label: "Dark"},
+    { value: "system", label: "System"},
+  ]
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {mode.map(({ value, label }) => (
+          <DropdownMenuItem key={value} onClick={() => setTheme(value)}>
+            {label}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
