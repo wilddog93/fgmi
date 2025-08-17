207e8830a5581ac735d6d402a2c4e4c6616f2718
Jumakri Ridho Fauzi <ridhoajibx@gmail.com>
2025-08-17 22:18:34 +0700

    [ridho] - fix notif api - WIP

--- src/app/api/payment/notification/route.ts
+++ src/app/api/payment/notification/route.ts
@@ -7,17 +7,61 @@ const coreApi = new midtransClient.CoreApi({
   clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY!,
 });
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { orderId: string } }
-) {
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const orderId = searchParams.get("order_id");
+
+  if (!orderId) {
+    return NextResponse.json({ error: "order_id required" }, { status: 400 });
+  }
+
   try {
-    const statusResponse = await coreApi.transaction.status(params.orderId);
+    const response = await fetch(
+      `https://api.sandbox.midtrans.com/v2/${orderId}/status`,
+      {
+        headers: {
+          Authorization:
+            "Basic " +
+            Buffer.from(process.env.MIDTRANS_SERVER_KEY + ":").toString(
+              "base64"
+            ),
+          "Content-Type": "application/json",
+        },
+        cache: "no-store",
+      }
+    );
+
+    const statusResponse = await response.json();
 
     console.log("Status response:", statusResponse);
 
     // Bisa update DB sesuai status transaksi
     // statusResponse.transaction_status: "capture", "settlement", "pending", "deny", "expire", "cancel"
+    if (statusResponse.transaction_status === "settlement") {
+      // Update DB
+      // Update status
+      // Send notification
+    }
+    if (statusResponse.transaction_status === "pending") {
+      // Update DB
+      // Update status
+      // Send notification
+    }
+    if (statusResponse.transaction_status === "deny") {
+      // Update DB
+      // Update status
+    }
+    if (statusResponse.transaction_status === "expire") {
+      // coreApi.cancelTransaction(orderId);
+      // Update DB
+      await coreApi.transaction.expire(orderId);
+    }
+    if (statusResponse.transaction_status === "cancel") {
+      // Update DB
+      // Update status
+      await coreApi.transaction.cancel(orderId);
+      // Send notification
+    }
 
     return NextResponse.json(statusResponse, { status: 200 });
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
